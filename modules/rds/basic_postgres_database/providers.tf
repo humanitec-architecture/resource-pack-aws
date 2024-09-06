364bc70a91ee7ddea4b0c587f4e7a868c179587b
Bobby DeVeaux <bobby.deveaux@riversafe.co.uk>
2024-09-06 10:21:37 +0100

    Provider again

--- modules/rds/basic_postgres_database/providers.tf
+++ modules/rds/basic_postgres_database/providers.tf
@@ -4,7 +4,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-    
+    postgresql = {
+      source  = "cyrilgdn/postgresql"
+      version = ">=1.22"
+    }
   }
 
   required_version = ">= 1.3.0"
